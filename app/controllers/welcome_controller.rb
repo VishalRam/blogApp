c1298c2c36180f72623b26935b78feb1f5f35f59
VishalRam <vishal.ramrakhyani@tinyowl.co.in>
2015-05-22 13:46:47 +0530

    1st commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+  def index
+    @message = "Hello, how are you today?"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '2053a91ece14aa6416f678fa9a5a7e774fff43df7ff03cbe3d76dab7616d8f14735eb79d868a6dda5b153823909e446d9bf74f085c42785d8b2631a3941be77e'
